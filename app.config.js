d1da30d675d4490c8c3a785e31bfcb400861f17b
Max Soe <maxsoe@gmail.com>
2018-03-20 14:35:11 +0000

    Using the "pinotNoir" theme

--- app.config.js
+++ app.config.js
@@ -1,8 +1,17 @@
 angular
   .module('insuranceApp')
   .config(function($mdThemingProvider) {
+
+    var pinotNoirMap = $mdThemingProvider.extendPalette('indigo', {
+       '500': '#182848',
+       'contrastDefaultColor': 'light'
+     });
+
+     // Register the new color palette map with the name <code>neonRed</code>
+     $mdThemingProvider.definePalette('pinotNoir', pinotNoirMap);
+
     $mdThemingProvider.theme('default')
-      .primaryPalette('indigo')
+      .primaryPalette('pinotNoir')
       .accentPalette('pink')
       .warnPalette('orange')
       .backgroundPalette('grey');
