da6bd6fa70f493fd0e31b1f0504eb3f72cb012e7
Max Soe <maxsoe@gmail.com>
2018-03-21 14:31:52 +0000

    Your policies - data

--- your-policies/your-policies.component.js
+++ your-policies/your-policies.component.js
@@ -13,5 +13,27 @@ angular
   });
 
 function yourPoliciesController() {
-
+  this.policies = [{
+      type: 'Landlord contents insurance',
+      expiry: '24 July 2018',
+      options: {
+        claims: ['897645321', '579438675']
+      }
+    },
+    {
+      type: 'Professional indemnity insurance',
+      value: '21 October 2018'
+    },
+    {
+      type: 'Unoccupied property insurance',
+      value: '25 December 2018',
+      options: {
+        claims: ['486257952']
+      }
+    },
+    {
+      type: 'Public liability insurance',
+      value: '8 February 2019'
+    }
+  ];
 };
