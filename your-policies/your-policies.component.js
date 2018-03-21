f3c7adc591c197a237762395ef07ef814e1192de
Max Soe <maxsoe@gmail.com>
2018-03-21 14:25:56 +0000

    "vert more" button in policies

--- your-policies/your-policies.component.js
+++ your-policies/your-policies.component.js
@@ -1,6 +1,10 @@
 // Register `yourPolicies` component, along with its associated controller and template
 angular
   .module('yourPolicies', [])
+  .config(function($mdIconProvider) {
+    $mdIconProvider
+      .iconSet('navigation', 'img/icon-set/navigation-icons.svg', 24)
+  })
   .component('yourPolicies', {
     templateUrl: 'your-policies/your-policies.template.html',
     css: 'your-policies/your-policies.css',
